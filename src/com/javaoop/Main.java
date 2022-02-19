7efa5ce695bf57a775c01629cd69d6483f24a468
AlibekPC <alibek.kasseinov@gmail.com>
2022-02-19 22:55:03 +0600

    Comktt

---
+++ src/com/javaoop/Main.java
@@ -0,0 +1,28 @@
+package com.javaoop;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        byte age = 45;
+        short num = 128;
+        int num2 = 56;
+        long num3 = 4567;
+
+        float num4 = 4.55453434f;
+        double num5 = 5.4343434;
+
+        char ch = 'e';
+        String user_name = "Alex";
+        System.out.println(user_name);
+
+        boolean isNasty = false;
+        System.out.println(isNasty);
+
+
+    }
+
+
+}
