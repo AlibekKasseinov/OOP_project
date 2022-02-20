56dbace8cfc0d5369cd8123adeccf6c61ee6da8e
alial <ali.altynbekov228@gmail.com>
2022-02-20 21:54:42 +0600

    Started work from localization of the server side of the Server class

---
+++ src/com/javaoop/Server.java
@@ -0,0 +1,34 @@
+package com.javaoop;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    /* Localization of the server side of the Server class */
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(port:8000);
+        System.out.println("Server started!");
+
+        Socket socket = server.accept();
+        System.out.println("Client connected");
+
+        BufferedWriter writer =
+                new BufferedWriter(
+                        new OutputStreamWriter(
+                                socket.getOutputStream()));
+
+        writer.write("Hello from Server");
+        writer.newLine();
+        writer.flush();
+
+        writer.close();
+        socket.close();
+        server.close();
+    }
+}
