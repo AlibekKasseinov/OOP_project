d970714262331408d4a98c35601a2d519a9af836
alial <ali.altynbekov228@gmail.com>
2022-02-22 00:14:56 +0600

    Now I'm doing exception handling

--- src/com/javaoop/Server.java
+++ src/com/javaoop/Server.java
@@ -1,9 +1,6 @@
 package com.javaoop;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,23 +10,32 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
 
-        ServerSocket server = new ServerSocket(port:8000); //Creating a Socket server
-        System.out.println("Server started!");
+        while (true)
+        try (ServerSocket server = new ServerSocket(port:8000)) //Creating a Socket server
+        { System.out.println("Server started!");
+            try (
 
-        Socket socket = server.accept(); //creating and saving a connection
-        System.out.println("Client connected");
+            Socket socket = server.accept(); //creating and saving a connection
 
-        BufferedWriter writer =
-                new BufferedWriter(
-                        new OutputStreamWriter(
-                                socket.getOutputStream()));
-
-        writer.write("Hello from Server");
-        writer.newLine();
-        writer.flush();
-
-        writer.close();
-        socket.close();
-        server.close();
+            BufferedWriter writer =
+                    new BufferedWriter(
+                            new OutputStreamWriter(
+                                    socket.getOutputStream()));
+            BufferedReader reader =
+            new BufferedReader (
+                    new InputStreamReader (
+                            socket.getInputStream()));
+            ) {
+                String request = reader.readLine();
+                System.out.println("Request" + request);
+                String response = "Hello from Server" + request.length();
+                System.out.println("Response " + response);
+                writer.write(response);
+                writer.newLine();
+                writer.flush();
+            }
+        } catch (IOException e) {
+            throw RuntimeException(e);
+        }
     }
 }
